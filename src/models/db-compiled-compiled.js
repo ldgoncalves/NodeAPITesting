e4120db463660d0befe9ca1888884a79ca5edf79
ldgoncalves <ldgoncalves@fullsail.edu>
2016-06-25 15:31:29 -0400

    Edited server file

--- src/models/db-compiled-compiled.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = required('sequelize');
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {});
-
-//# sourceMappingURL=db-compiled.js.map
-
-//# sourceMappingURL=db-compiled-compiled.js.map
